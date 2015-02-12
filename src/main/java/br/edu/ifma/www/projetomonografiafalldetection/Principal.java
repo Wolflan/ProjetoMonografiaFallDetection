55e669274160f84c919222bcab5887cab26ebc9a
wolflan <wolflanz@hotmail.com>
2015-02-12 17:27:26 -0200

    Initial commit

---
+++ src/main/java/br/edu/ifma/www/projetomonografiafalldetection/Principal.java
@@ -0,0 +1,98 @@
+package br.edu.ifma.www.projetomonografiafalldetection;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class Principal extends Activity implements SensorEventListener{
+
+    //Variáveis para controle do Acelerômetro
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private boolean min = false;
+    private boolean max = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_principal);
+        //Criando os objetos para controle do sensor
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Setando o Listener do sensor
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Libera o Listener evitando consumo de processamento e bateria.
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        boolean queda = fallDetection(event);
+
+        if (queda)
+            Toast.makeText(this,"Queda!",Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean fallDetection (SensorEvent event) {
+        Float x = event.values[0];
+        Float y = event.values[1];
+        Float z = event.values[2];
+
+        //Determina o módulo / norma, comprimento do vetor (V³)
+        Double resultado = Math.sqrt(Math.abs(x*x) + Math.abs(y*y) + Math.abs(z*z));
+
+        //Lower Threshold  0G (Free Fall)
+        if (resultado <= 2) {
+            min = true;
+        }
+        //Upper Threshold 3G, Impact!
+        if (resultado >= 15) {
+            max = true;
+        }
+
+        //Lower and Upper Detected!
+        if (min && max) {
+            //Inactivity 1G
+            if (resultado >= 9 && resultado <= 9.8) {
+
+                min = false;
+                max = false;
+
+                return true;
+            }
+        }
+
+        return  false;
+    }
+
+    private boolean deviceCaido(Float [] vetor) {
+
+        long t0 = System.currentTimeMillis();
+        return false;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
